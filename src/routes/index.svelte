f8ddbbd37241ff4ca65257abcab8c324dccc3d1f
jbailey111 <jake.bailey2599@gmail.com>
2021-12-14 02:53:22 +0000

    Added Css layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,18 @@
 <script>
 	import TodoItem from '$lib/todo-item.svelte';
-
-    const title = "Todo List"
+	const title = 'Todo List';
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
-<form action="" method="">
-	<input type="text" placeholder="+ Add a new Todo" name="text" aria-label="Add a todo item" />
-	<TodoItem />
-</form>
+<div id="container">
+	<h1>{title}</h1>
+	<div id="TodoItem">
+		<form action="" method="">
+			<input type="text" placeholder="+ Add a new Todo" name="text" aria-label="Add a todo item" />
+			<TodoItem />
+		</form>
+	</div>
+</div>
