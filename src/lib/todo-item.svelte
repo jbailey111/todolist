fb26b07dcd8939a87a353b2e74e88c850558f177
jbailey111 <jake.bailey2599@gmail.com>
2021-12-15 01:47:56 +0000

    Css on todo list

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,13 +1,65 @@
-<div>
+<div class="todowrapper">
 	<form action="" method="">
 		<input type="hidden" name="done" value="" />
-		<button aria-label="Mark done/not done">Done/NotDone</button>
+		<button class="toggle" aria-label="Mark done/not done">Done/NotDone</button>
 	</form>
-	<form action="" method="">
+	<form action="" method="" class="text">
 		<input type="text" />
-		<button aria-label="Save todo">Save</button>
+		<button class="save" aria-label="Save todo">Save</button>
 	</form>
 	<form action="" method="">
-		<button aria-label="Delete todo">Delete</button>
+		<button class="deletebtn" aria-label="Delete todo">Delete</button>
 	</form>
 </div>
+
+<style>
+	.todowrapper {
+		display: grid;
+		grid-template-columns: 2rem 1fr 2rem;
+		grid-gap: 0.5em;
+		align-items: center;
+		margin: 0 0 0.5rem 0;
+		padding: 0.5rem;
+		background-color: white;
+		border-radius: 8px;
+		filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
+		transform: translate(-1px, -1px);
+		transition: filter 0.2s, transform 0.2s;
+	}
+	.todowrapper button {
+		width: 2em;
+		height: 2em;
+		border: none;
+		background-color: transparent;
+		background-position: 50% 50%;
+		background-repeat: no-repeat;
+	}
+
+	.todowrapper input {
+		flex: 1;
+		padding: 0.5em 2em 0.5em 0.8em;
+		border-radius: 3px;
+	}
+
+	.toggle {
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		border-radius: 50%;
+
+		box-sizing: border-box;
+		background-size: 1em auto;
+	}
+
+	.text {
+		position: relative;
+		display: flex;
+		align-items: center;
+		flex: 1;
+	}
+
+	.save{
+		position: absolute;
+		right: 0;
+		opacity: 0;
+		background-image: url();
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,32 +3,45 @@
 	const title = 'Todo List';
 </script>
 
-<style>
-	.new{
-		margin: 0 0 0.5rem 0;
-	}
-	.new input{
-font-size: 28px;
-width:100%;
-padding: 0.5em 1em 0.3em 1em;
-box-sizing: border-box;
-border-radius: 4px;
-background: none;
-text-align: center;
-	}
-
-</style>
-
 <svelte:head>
 	<title>{title}</title>
 </svelte:head>
 
 <div id="container">
-	<h1>{title}</h1>
+	<h1 class="title">{title}</h1>
+	<form action="" method="" class="new">
+		<input type="text" placeholder="+ Add a new Todo" name="text" aria-label="Add a todo item" />
+	</form>
 	<div id="TodoItem">
-		<form action="" method="" class="new">
-			<input type="text" placeholder="+ Add a new Todo" name="text" aria-label="Add a todo item" />
-			<TodoItem />
-		</form>
+		<TodoItem />
+		<TodoItem />
+		<TodoItem />
+		<TodoItem />
 	</div>
 </div>
+
+<style>
+	.title {
+		margin-bottom: 1em;
+	}
+	.new {
+		margin: 0 0 0.5rem 0;
+		width: 100%;
+	}
+	.new input {
+		font-size: 28px;
+		width: 100%;
+		padding: 0.5em 1em 0.3em 1em;
+		box-sizing: border-box;
+		border-radius: 4px;
+		background: none;
+		text-align: center;
+		border: 1px solid transparent;
+		margin-bottom: 1em;
+	}
+	/* .container :global(input:focus-visible) {
+		box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.1);
+		border: 1px solid rgb(0, 59, 252) !important;
+		outline: none;
+	} */
+</style>
